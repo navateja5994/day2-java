e71985534b393c1fd1db4fa1cf79095043116800
navateja5994 <navatejar80@gmail.com>
2024-12-03 10:23:50 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,14 @@
+class a
+{
+    public static void main(String args[])
+    {
+        int a=54;
+        int b=45;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+}
