0da6072b3966247ad29f9fecdbe5410ee946991d
navateja5994 <navatejar80@gmail.com>
2024-12-03 10:16:15 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Arithmetic
+{
+    public static void main(String args[])
+{
+    int a=5;
+    int b=3;
+    System.out.println("Addition operation:"+(a+b));
+    System.out.println("Subtraction operation:"+(a-b));
+    System.out.println("Multiplication operator:"+(a*b));
+}
+}
