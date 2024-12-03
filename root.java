f6ea1bc86d56a4aa6c1467297595685d81f2370d
navateja5994 <navatejar80@gmail.com>
2024-12-03 15:18:18 +0530

    Add files via upload

---
+++ root.java
@@ -0,0 +1,21 @@
+class GFG {
+ 
+    public static int square(int n)
+    {
+     if (n < 0)
+            n = -n;
+ 
+        
+        int res = n;
+        for (int i = 1; i < n; i++)
+            res += n;
+ 
+        return res;
+    }
+     public static void main(String[] args)
+    {
+    for (int n = 1; n <= 5; n++)
+            System.out.println("n = " + ", n^2 = " + square(n));
+    }
+}
+ 
