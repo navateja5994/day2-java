7320ba632e66004b2c029253413c9c0238e394f1
navateja5994 <navatejar80@gmail.com>
2024-12-03 11:03:28 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,15 @@
+class swap
+{
+    public static void main(String args[])
+    {
+        int a=32;
+        int b=65;
+        int c;
+        c=b;
+        b=a;
+        a=c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
