f3b1120bc4e4a739e7acbfa1e36a56145446b5ba
navateja5994 <navatejar80@gmail.com>
2024-12-03 16:10:52 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,16 @@
+class args
+{
+    public static void main(String args[])
+    {
+        int a=34;
+        int b=67;
+        if((a&b)==0)
+        {
+            System.out.println("EVen");
+        }
+        else
+        {
+            System.out.println("ODD");
+        }
+    }
+}
