619bd390b6e92da2934865974b9ece8f24283af9
navateja5994 <navatejar80@gmail.com>
2024-12-03 10:54:06 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,12 @@
+class ass
+{
+    public static void main(String args[])
+    {
+        int a=23;
+        System.out.println(a+=30);
+        System.out.println(a);
+        System.out.println(a-=4);
+        System.out.println(a*=3);
+        System.out.println(a/=4);
+    }
+}
